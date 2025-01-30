d478c6eb7af855747366c40d84d16f7cad4782de
Bella Ball <mohawkball@gmail.com>
2025-01-30 13:14:39 -0600

    do exercise 6 + 7

--- Chapter02/Exercise06/Exercise06.cpp
+++ Chapter02/Exercise06/Exercise06.cpp
@@ -5,30 +5,31 @@
 int main()
 {
     std::string input;
-    int number;
-    
-    std::cout << "Menu\n";
-    std::cout << "1: Fries\n";
-    std::cout << "2: Burger\n";
-    std::cout << "3: Shake\n";
-    std::cout << "Please enter a number 1-3 to view an item price: ";
-    getline (std::cin, input);
-    number = std::stoi(input);
-    if (number == 1)
-    {
-        std::cout << "Fries: $0.99\n";
+    int numb;
+
+    std::cout << "1. Burger\n";
+    std::cout << "2. Fries\n";
+    std::cout << "3. Shake\n";
+    std::cout << "4. Soda\n";
+
+    std::cout << "Select your choice with a number 1-4: ";
+    getline(std::cin, input);
+    numb = std::stoi(input);
+
+    if (numb == 1) {
+        std::cout << "Burger $5.95";
     }
-    else if (number == 2) 
-    {
-        std::cout << "Burger: $1.25\n";
+    else if (numb == 2) {
+        std::cout << "Fries $2.95";
     }
-    else if (number == 3)
-    {
-        std::cout << "Shake: $1.50\n";
+    else if (numb == 3) {
+        std::cout << "Shake $3.95";
     }
-    else
-    {
-        std::cout << "Invalid choice.";
-    }  
+    else if (numb == 4) {
+        std::cout << "Soda $1.99";
+    }
+    else {
+        std::cout << "Invalid choice - not on menu";
+    }
     return 0;
 }
