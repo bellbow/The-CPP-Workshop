a1a05c9a32b9dbaa910828c4ff2481917b148507
Bella Ball <mohawkball@gmail.com>
2025-01-30 18:16:53 -0600

    Do Activity 2

--- Chapter02/Activity02/Activity02.cpp
+++ Chapter02/Activity02/Activity02.cpp
@@ -2,60 +2,59 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
-    // Declare variables.
-    int guessCount = 0;
-    int minNumber = 0;
-    int maxNumber = 0;
-    int randomNumber = 0;
+int main() {
+    //Variables
     std::string input = "";
     bool bIsRunning = true;
-    
-    while (bIsRunning)
-    { 
-        // Output instructions and get user inputs.
-        std::cout << "***Number guessing game***\n";
-        std::cout << "\nEnter the number of guesses: ";
+    int guessNum = 0;
+    int maxNum = 0;
+    int minNum = 0;
+    int randomNumber = 0;
+
+    //Running Loop
+    while (bIsRunning) {
+        //Get user input
+        std::cout << "****Let's Play A Number Guessing Game****\n";
+        std::cout << "Enter a number of guesses: ";
         getline(std::cin, input);
-        guessCount = std::stoi(input);
-        
-        std::cout << "Enter the miniumum number: ";
+        guessNum = std::stoi(input);
+
+        std::cout << "Enter a maximum number: ";
         getline(std::cin, input);
-        minNumber = std::stoi(input);
-        
-        std::cout <<"Enter the maximum number: ";
+        maxNum = std::stoi(input);
+
+        std::cout << "Enter a minimum number: ";
         getline(std::cin, input);
-        maxNumber = std::stoi(input);
-        
-        // Generate random number within range.
+        minNum = std::stoi(input);
+
+        //Generate random number
         srand((unsigned)time(0));
-        randomNumber = rand() % (maxNumber - minNumber + 1) + minNumber;
+        randomNumber = rand() % (maxNum - minNum + 1);
         
-        // Process user guesses.
-        for (int i = 0; i < guessCount; ++i)
-        {
+        //Guessing loop
+        for  (int i = 0; i < guessNum; ++i) {
             int guess = 0;
             
-            std::cout << "\nEnter your guess: ";
+            std::cout << "\nGuess the number: ";
             getline(std::cin, input);
             guess = std::stoi(input);
-            
-            if (guess == randomNumber)
-            {
-                std::cout << "Well done, you guessed the number!\n";
+
+            //End if correct
+            if (guess == randomNumber) {
+                std::cout << "You got it! Good job!\n";
                 break;
             }
-            
-            int guessesRemaining = guessCount - (i + 1);
-            std::cout << "Your guess was too " << (guess < randomNumber ? "low. " : "high. ");
-            std::cout << "You have " << guessesRemaining << (guessesRemaining > 1 ? " guesses" : " guess") << " remaining";
+
+            //Reduce guesses and give hint
+            int guessesRemaining = guessNum - (i + 1);
+            std::cout << "Try " << (guess > randomNumber ? "lower" : "higher") << ".\n"; 
+            std::cout << "You have " << guessesRemaining << " guesses left. \n";  
         }
-        
-        std::cout << "\nEnter 0 to exit, or any number to play again: ";
-        getline(std::cin, input);
-        if (std::stoi(input) == 0)
-        {
+
+        std::cout << "Enter 0 to exit, any number to continue: ";
+        getline(std::cin,input);
+
+        if (std::stoi(input)==0) {
             bIsRunning = false;
         }
     }
